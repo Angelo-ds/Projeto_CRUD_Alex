19399e102c19bb08b57e49ee2dc2c8d66f8888b7
Gabriel Belentani Felipe <gabriel.portifolio07@gmail.com>
2025-10-30 14:48:02 -0300

    add: adicionei o criar e listar projetos

--- database.py
+++ database.py
@@ -16,3 +16,24 @@ def conectar():
     conn.commit()
     return conn
 
+import sqlite3
+
+
+def inserir_projeto(nome, cliente, prazo, valor, status):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO projetos (nome_projeto, cliente, prazo_entrega, valor, status) VALUES (?, ?, ?, ?, ?)",
+                   (nome, cliente, prazo, valor, status))
+    conn.commit()
+    conn.close()
+
+def listar_projetos():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM projetos")
+    projetos = cursor.fetchall()
+    conn.close()
+    return projetos
+
+
+
